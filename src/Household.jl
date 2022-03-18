8c4ac817d984250a8303f709afc3f4100f1ba9d5
Atiyah <a.m.g.elsheikh@gmail.com>
2022-03-18 15:43:09 +0000

    Town module

--- src/Household.jl
+++ src/Household.jl
@@ -1,5 +1,6 @@
 module Household
 
   # type HouseHold to extend AbstractAgentsGroup
+  # It is a collection of persons associated with a particular house
   
 end 
---
+++ src/Town.jl
@@ -0,0 +1,4 @@
+module Town
+    # Type Town extends the type  AbstractAgentsGroup 
+    # a collection of houses 
+end 
