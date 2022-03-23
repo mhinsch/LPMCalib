dce395b3c2c67722f4760a3726122b7ac2ec396d
Atiyah <a.m.g.elsheikh@gmail.com>
2022-03-23 16:03:13 +0000

    a way to design types from abstract types

--- src/Agent.jl
+++ src/Agent.jl
@@ -26,6 +26,16 @@ module Agent
     const Person = AbstractAgentData{PersonData} 
     =# 
 
+    #= 
+    Common functions can be defined here, e.g.
+    function foo() = "foo"                           # meaningless dummy implementation  
+    function goo() end                               # empty function 
+    function hoo = error("hoo shall not be called")  # should not be called for subtype's instances 
+    function zoo()                                   # common function for subtype's instances 
+        # do something
+    end 
+    =# 
+
     include("House.jl")
 
     include("Person.jl")
