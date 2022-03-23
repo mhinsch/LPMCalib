03362aa0487da43296f2c27ba312b735c9396de7
Atiyah <a.m.g.elsheikh@gmail.com>
2022-03-23 11:43:13 +0000

    src

--- src/Agent.jl
+++ src/Agent.jl
@@ -1,6 +1,30 @@
 module Agent
     
-    # declare the type AbstractAgent 
+    abstract type AbstractAgent end 
+    
+    # Abstract types in Julia come without data fields (i.e. they rather define common behavior)
+    # It could be possible to have struct of common data entry in any agent, e.g. 
+    #=
+
+    mutable struct AbstractAgentData{A <: AbstractAgent}
+        Int64::id  
+        A 
+    end
+
+    mutable struct HouseData <: AbstractAgent
+        location 
+        size
+        ... 
+    end
+
+    mutable struct PersonData <: AbstractAgent 
+        father 
+        mother 
+    end
+
+    const House  = AbstractAgentData{HouseData} 
+    const Person = AbstractAgentData{PersonData} 
+    =# 
 
     include("House.jl")
 
