d77c2ceeb01061dcf957aef4a0255f0ebaf538f0
Atiyah <a.m.g.elsheikh@gmail.com>
2022-03-30 10:05:58 +0100

    bug

--- src/Household.jl
+++ src/Household.jl
@@ -2,9 +2,7 @@ import AgentTypes: Person
 
 export Household
 
-"""
-   Household: group of persons associated with a particular house.
-"
+"Household: group of persons associated with a particular house."
 const Household = Group{Person}
 
 # A specific constructor that verify all persons in a household are related or associated
