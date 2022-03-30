25995d4e895921b82bce960cdd78df39027f2c12
Atiyah <a.m.g.elsheikh@gmail.com>
2022-03-30 08:53:56 +0100

    remove unnecessary comments

--- src/House.jl
+++ src/House.jl
@@ -1,26 +1,19 @@
 """
 Specification of a House Agent Type. This file is included in the module AgentTypes
 Type House to extend from AbstractAgent.
-
-To test the code in this file seperate from the parent module, 
-use the following code 
-
-if not Symbol(:AgentTypes) in names(parentmodule(House),imported=true)
-    # using AgentTypes The same as below
-    import AgentTypes: AbstractAgent, Agent, DataSpec
-end 
 """ 
+
+#=
+If the code in this file to be included seperately, use the following imports 
+
+import AgentTypes: AbstractAgent, Agent, DataSpec
+=# 
 
 export  House, HouseData
 
 #= 
 Does house data need to be mutable during a simulation course?
 if yes, then declare the following struct as mutable 
-[mutable] struct HouseData <: DataSpec
-location 
-size
-... 
-end
 =# 
 
 """
