c75a21dc52b8f7aaa4c2a201bc14254d2f007192
Atiyah <a.m.g.elsheikh@gmail.com>
2022-04-06 10:32:13 +0100

    documentation on how to run tests

--- tests/RunTests.jl
+++ tests/RunTests.jl
@@ -1,3 +1,13 @@
+"""
+Run this script from shell as 
+$ JULIA_LOAD_PATH="/path/to/LoneParentsModel.jl/src:$JULIA_LOAD_PATH" julia RunTests.jl
+
+or with REPL
+
+julia> push!(LOAD_PATH,"/path/to/...")
+julia> include("RunTests.jl")
+"""
+
 using AgentTypes, GroupTypes, Test
 
 @testset verbose=true "Basic declaration" begin
@@ -8,10 +18,14 @@ using AgentTypes, GroupTypes, Test
     person2 = person1 
     @test person1 === person2
     person3 = Person() 
-    @test person3 != person2 skip=true  
+    # The following test will fail since at the moment the person class is not implemented
+    # skip implies that the test is broken indicating a non-implemented functionality
+    @test person3 != person2 skip=true 
+    @test person3 != person2 broken=true               
     @test (pop = Population()) != nothing 
     @test household = Household() != nothing 
-    @test_throws UndefVarError town = Town()
+    @test_throws UndefVarError town = Town() 
+    @test town = Town() skip=true 
 end 
 
 # detect_ambiguities(AgentTypes)
