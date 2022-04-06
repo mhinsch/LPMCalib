ae3c66dd352ced6369ab73f557fda1639b712e04
Atiyah <a.m.g.elsheikh@gmail.com>
2022-04-06 11:52:24 +0100

    document some tests

--- tests/RunTests.jl
+++ tests/RunTests.jl
@@ -1,8 +1,8 @@
 """
 Run this script from shell as 
-$ JULIA_LOAD_PATH="/path/to/LoneParentsModel.jl/src:$JULIA_LOAD_PATH" julia RunTests.jl
+# JULIA_LOAD_PATH="/path/to/LoneParentsModel.jl/src:\$JULIA_LOAD_PATH" julia RunTests.jl
 
-or with REPL
+or within REPL
 
 julia> push!(LOAD_PATH,"/path/to/...")
 julia> include("RunTests.jl")
@@ -12,19 +12,27 @@ using AgentTypes, GroupTypes, Test
 
 @testset verbose=true "Basic declaration" begin
     person1 = Person() 
-    @test person1  != nothing 
+    @test person1  != nothing
+    @test person.id > -1 skip=true                      # every person should have an ID 
+
     @test (house  = House())  != nothing
-    @test_throws MethodError person2 = Person(person1)
-    person2 = person1 
+
+    @test_throws MethodError person2 = Person(person1)  # copy constructor should not be implm,ented 
+    
+    person2 = person1                                   # This is just an alais 
     @test person1 === person2
+
     person3 = Person() 
     # The following test will fail since at the moment the person class is not implemented
     # skip implies that the test is broken indicating a non-implemented functionality
-    @test person3 != person2 skip=true 
+    @test person3 != person2 skip=true                 # A new person is another person    
     @test person3 != person2 broken=true               
-    @test (pop = Population()) != nothing 
-    @test household = Household() != nothing 
-    @test_throws UndefVarError town = Town() 
+
+    @test (pop = Population()) != nothing skip=true    # Population means something 
+
+    @test household = Household() != nothing           # a household instance is something 
+
+    @test_throws UndefVarError town = Town()           # Town class is not yet implemented 
     @test town = Town() skip=true 
 end 
 
