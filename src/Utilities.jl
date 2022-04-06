2ce198772f9ed39613bae405908f7780f2ac6693
Atiyah <a.m.g.elsheikh@gmail.com>
2022-04-06 11:53:47 +0100

    Some useful functions

---
+++ src/Utilities.jl
@@ -0,0 +1,23 @@
+"""
+Diverse useful functions 
+"""
+module Utilities
+
+    export 
+
+    # useful built-in functions: 
+    # function ispath(str) check the existance of a file or a directory 
+    # isdir(str), isfile(str)
+
+    # 
+    # reading and writing a file 
+    # try 
+    #   fin = fopen("input.txt",mode)
+    # catch exc
+    #   println("$exc")
+    # finally
+    #   close(fin)
+    # 
+    
+    # run(command)
+end 
