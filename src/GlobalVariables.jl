76ed3fcba302af1c65b2661f32d0668a704a1e51
Atiyah <a.m.g.elsheikh@gmail.com>
2022-05-04 07:33:34 +0100

    rename

---
+++ src/GlobalVariables.jl
@@ -0,0 +1,13 @@
+# Set of all global variables shared among modules 
+module GlobalVariables
+
+    export SimulationFolderPrefix
+
+    # list of global variables
+
+    "Folder in which simulation results are stored"
+    const SimulationFolderPrefix = "Simulations_Folder"
+    
+    # timeStamp ... 
+
+end
