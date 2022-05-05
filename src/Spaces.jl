88d794f242e0749bf18d75f16de7b0f77872005d
Atiyah <a.m.g.elsheikh@gmail.com>
2022-05-05 17:49:42 +0100

    Initial implemtation of GridSpace / TownLocationType

--- src/Spaces.jl
+++ src/Spaces.jl
@@ -4,6 +4,14 @@ A set of spaces on which common agents and ABMs are operating
 
 modules Spaces 
 
-    # GridSpace Tuble of size 4
+    export GridSpace 
+
+    struct GridSpace 
+        gridDimension::NTuple{D,Int} where D  
+    end 
+ 
+    function createTownLocationType(x,y)
+        GridSpace((x,y))
+    end
 
 end # Spaces 
