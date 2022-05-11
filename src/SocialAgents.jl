15b496804c2b57174b9a6a626f70089f914648e3
Atiyah <a.m.g.elsheikh@gmail.com>
2022-05-11 09:49:08 +0100

    Isolate AbstractAgent concept into a single file

--- src/SocialAgents.jl
+++ src/SocialAgents.jl
@@ -1,44 +1,10 @@
-"Definitions of supertypes for all Agent types."
+"""
+Module for defining a supertype, AbstractAgent for all Agent types
+
+"""
 module SocialAgents
 
-    export AbstractAgent
-    export agent_step!, getindex, getposition 
-    export getProperty, setProperty!
-
-    "Number of instantiated agents"
-    global IDCOUNTER = 0::Int64                           
-    
-    "Supertype of any Agent type"
-    abstract type AbstractAgent end     
-    
-    # "A list of all agents"
-    # allAgentsList::Array{AbstractAgent,1} = AbstractAgent[] 
-    # does not work ..
-    # TODO struct for mapping agents ids to agents 
-    # ...
-    
-    "Any agent should have an ID number"
-    getindex(A::AbstractAgent) = A.id 
-
-    "Any agent should be assigned to a location"
-    getposition(A::AbstractAgent)  = A.pos
-    
-    "Set an agent field using this function"
-    function setProperty!(agent::AbstractAgent,
-                         property::Symbol,
-                         val) 
-        setfield!(agent,property,val)
-    end
-
-    function getProperty(agent::AbstractAgent, 
-                         property::Symbol)
-        getfield(agent,property)
-    end
-
-    #=
-    Base.show(io,agent::AbstractAgent) = ... 
-    =# 
-
+    include("./agents/AbstractAgent.jl")    
     include("./agents/Town.jl")
     include("./agents/House.jl")
     include("./agents/Person.jl")
---
+++ src/agents/AbstractAgent.jl
@@ -0,0 +1,31 @@
+export AbstractAgent
+export agent_step!, getindex, getposition 
+export getProperty, setProperty!
+
+"Number of instantiated agents"
+global IDCOUNTER = 0::Int64                           
+    
+"Supertype of any Agent type"
+abstract type AbstractAgent end     
+    
+"Any agent should have an ID number"
+getindex(A::AbstractAgent) = A.id 
+
+"Any agent should be assigned to a location"
+getposition(A::AbstractAgent)  = A.pos
+    
+"Set an agent field using this function"
+function setProperty!(agent::AbstractAgent,
+                      property::Symbol,
+                      val) 
+    setfield!(agent,property,val)
+ end
+
+ function getProperty(agent::AbstractAgent, 
+                      property::Symbol)
+    getfield(agent,property)
+ end
+
+ #=
+ Base.show(io,agent::AbstractAgent) = ... 
+ =# 
