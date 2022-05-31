c76390793ba310b53ce64fcfc2067890461a1905
Atiyah <a.m.g.elsheikh@gmail.com>
2022-05-31 14:53:03 +0100

    comment

--- src/agents/AbstractSocialAgent.jl
+++ src/agents/AbstractSocialAgent.jl
@@ -1,5 +1,13 @@
 """
 Specification of an abstract agent supertype 
+
+The idea is to add an additional layer of absraction that distinguishes 
+declared agents from the library Agents.jl to the sophisticated agents 
+we want to employ for sophisticated socio-econmic ABMs. This is to allow 
+extension of Agents.jl (without modifying it) 
+
+AbstractAgent could be replaced by using Agents.jl in case Agents.jl will turn 
+to be directly usable
 """
 
 export AbstractAgent, AbstractSocialAgent 
@@ -10,7 +18,8 @@ global IDCOUNTER = 0::Int64              # This is differnt than agents.jl
                                                  
 "Supertype of any Agent type"
 abstract type AbstractAgent end          # to be replaceable by > using Agents.jl 
-    
+  
+"Specific abstract type for the type of agent exampels to be modelled using this package" 
 abstract type AbstractSocialAgent <: AbstractAgent end
 
 function verify(a::AbstractSocialAgent) 
