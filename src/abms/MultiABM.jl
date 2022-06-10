57003b00f0f8bb3d9f42d299e0fc7e80ad1bae8c
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-10 18:45:55 +0100

    initialization concept

--- src/abms/MultiABM.jl
+++ src/abms/MultiABM.jl
@@ -7,8 +7,12 @@
 
 export MultiABM 
 
+abstract type AbstractMABM  end   # <: AbstractSocialABM (to think about it)
+
+dummyinit(mabm::AbstractMABM) = nothing 
+
 "A MultiABM concept" 
-mutable struct MultiABM # <: AbstractSocialABM (to think about it)
+mutable struct MultiABM   <: AbstractMABM 
     abms::Array{AbstractSocialABM,1} 
 
     "Dictionary of model properties"
@@ -19,8 +23,14 @@ mutable struct MultiABM # <: AbstractSocialABM (to think about it)
         a list of elemantary ABMs together with
         MABM-level properties  
     """  
-    MultiABM(properties::Dict{Symbol} = Dict{Symbol,Any}(); 
-             declare::Function) =  new(declare(properties),copy(properties))     
+    function MultiABM(properties::Dict{Symbol} = Dict{Symbol,Any}(); 
+                    initialize::Function = dummyinit,  
+                    declare::Function) 
+        mabm = new(declare(properties),copy(properties))
+        initialize(mabm) 
+        mabm
+    end 
+
 end # MultiABM  
 
 ### ^^^ attach stepfunction!
