9b8a26a2e04a5d23c49e092783995ec8dbe38270
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-21 09:38:22 +0100

    rename

--- src/lpm/Simulate.jl
+++ src/lpm/Simulate.jl
@@ -3,20 +3,27 @@
 """ 
 
 
-module DemographySim
+module Simulate
 
 using Utilities: age2yearsmonths
 
 function doDeaths!(population::SocialABM{Person}) # agent_step / model_step? 
 
     (curryear,currmonth) = age2yearsmonths(demography.properties[:currstep])
-    currmonth = currmonth + 1
+    currmonth = currmonth + 1 
 
-    livingPeople = nothing  
+    people = allagents(population)
+    livingPeople = [person for person in people if person.info.alive]
+
+    @assert typeof(person.info.age) == Rational 
 
     for person in livingPeople
 
         if curryear >= 1950 
+
+            person.info.age = person.info.age > 109 ? Rational(109) : nothing 
+            rawRate = isMale(person)   ?  
+            rawRate = isFemale(person) ? 
 
         else # curryear < 1950 
 
@@ -25,4 +32,4 @@ function doDeaths!(population::SocialABM{Person}) # agent_step / model_step?
     end # for livingPeople 
 end
 
-end # DemographySim
+end # Simulate 
