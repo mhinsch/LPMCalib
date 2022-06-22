c514b01a62a4caf02c4db9f16672282e77c75de6
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-22 11:51:45 +0100

    Include Simulate Module

--- src/LoneParentsModel.jl
+++ src/LoneParentsModel.jl
@@ -3,6 +3,7 @@ module LoneParentsModel
     include("./lpm/Loaders.jl")
     include("./lpm/Declare.jl")
     include("./lpm/Initialize.jl")
+    include("./lpm/Simulate.jl")
     include("./lpm/SimSetup.jl")
 
 end # LoneParentsModel
