84668284aa38cf60986fbc92742157ed63550c72
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-22 16:14:57 +0100

    under progress

--- src/lpm/SimSetup.jl
+++ src/lpm/SimSetup.jl
@@ -5,7 +5,7 @@ using SocialABMs: dummystep, defaultprestep!, defaultpoststep!
 using SocialSimulations: startTime, dt
 using SocialSimulations: ABMSocialSimulation, MABMSimulation
 using SocialSimulations: attach_agent_step!, attach_pre_model_step!, attach_post_model_step!  
-
+using LoneParentsModel.Simulate: doDeaths!
 import SocialSimulations: setup!, AbstractExample
 
 export LPMUKDemography
@@ -34,17 +34,20 @@ end
 
 
 function setup!(sim::ABMSocialSimulation,example::LPMUKDemography) 
-    attach_agent_step!(sim,dummystep) 
-    attach_pre_model_step!(sim,defaultprestep!)
-    attach_post_model_step!(sim,defaultpoststep!)
-    sim.model.properties[:currstep] = startTime(sim) 
-    sim.model.properties[:dt]       = dt(sim)
+    
+    sim.agent_steps      = [dummystep]  
+    sim.pre_model_steps  = [defaultprestep!]
+    sim.post_model_steps = [defaultpoststep!] 
+
+    sim.model.properties[:currstep]   = Rational(startTime(sim)) 
+    sim.model.properties[:dt]         = dt(sim)
     sim.model.properties[:stepnumber] = 0 
     nothing 
 end
 
 function setup!(sim::MABMSimulation,example::LPMUKDemography) 
     attach_agent_step!(sim.simulations[3],agestep!)    
+    attach_pre_model_step!(sim.simulations[3],doDeaths!)
     #= 
     n = length(sim.simulations) 
     for i in 1:n 
--- src/lpm/Simulate.jl
+++ src/lpm/Simulate.jl
@@ -5,27 +5,57 @@
 
 module Simulate
 
+using SocialAgents: Person 
+using SocialAgents: isMale, isFemale
+using SocialABMs: SocialABM, allagents
 using Utilities: age2yearsmonths
+export doDeaths!
 
 function doDeaths!(population::SocialABM{Person}) # agent_step / model_step? 
 
-    (curryear,currmonth) = age2yearsmonths(demography.properties[:currstep])
+    parameters = population.properties 
+
+    (curryear,currmonth) = age2yearsmonths(Rational(population.properties[:currstep]))
     currmonth = currmonth + 1 
 
     people = allagents(population)
     livingPeople = [person for person in people if person.info.alive]
 
-    @assert typeof(person.info.age) == Rational 
+    #=
+    println("living people $(length(livingPeople)) sample \n : ") 
+    println(livingPeople[1:10]) 
+    println(livingPeople[1].info.age)
+    println(typeof(livingPeople[1].info.age))
+    @assert typeof(livingPeople[1].info.age) == Rational{Int64} 
+    sleep(1)
+    =# 
 
     for person in livingPeople
 
+        @assert isMale(person) || isFemale(person) # Assumption 
+        age = person.info.age 
+
         if curryear >= 1950 
 
-            person.info.age = person.info.age > 109 ? Rational(109) : nothing 
-           # rawRate = isMale(person)   ?  
-           # rawRate = isFemale(person) ? 
+           age = age > 109 ? Rational(109) : person.info.age 
+           rawRate = isMale(person) ? population.data[:death_male] : 
+                                      population.data[:death_female]
+           
+           error("translation incomplete for year >= 1950 ")
 
         else # curryear < 1950 
+
+            babyDieProb = age < 1 ? parameters{:babyDieProb} : 0.0 
+            ageDieProb  = isMale(person) ? 
+                            exp(age / parameters[:maleAgeScaling])  * parameters[:maleAgeDieProb] : 
+                            exp(age / parameters[:femaleAgeScaling]) * parameters[:femaleAgeDieProb]
+            rawRate = parameters[:baseDieProb] + babyDieProb + ageDieProb
+
+            #=
+            Not realized yet: 
+            classPop = [x for x in self.pop.livingPeople 
+                              if x.careNeedLevel == person.careNeedLevel]
+            =#
 
         end
         
