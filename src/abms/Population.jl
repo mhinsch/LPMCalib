804c060c065b9bf8ddafc54c82e140aceac5bb0a
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-23 17:57:06 +0100

    removeDead agent step function for focusing only on alive people

--- src/abms/Population.jl
+++ src/abms/Population.jl
@@ -5,7 +5,7 @@ This file is included with SocialABMs module. This file is subject to removal or
 """
 
 using  SocialAgents: Person
-import SocialAgents: agestep!, agestepAlivePerson! 
+import SocialAgents: agestep!, agestepAlivePerson!, removeDead! 
 
 export population_step!, agemonthstep!, agestepAlivePerson
 
@@ -16,6 +16,12 @@ function population_step!(population::SocialABM{Person};dt=1//12)
         agestep!(agent,dt=dt)
     end
 end 
+
+"remove dead persons" 
+function removeDead!(person::Person, population::SocialABM{Person}) 
+    person.info.alive ? nothing : kill_agent!(person, population) 
+    nothing 
+end
 
 "increment age with the simulation step size"
 agestep!(person::Person,population::SocialABM{Person}) = agestep!(person,dt=population.properties[:dt])
