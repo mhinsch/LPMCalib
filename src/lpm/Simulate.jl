d2a25483ffd95a7858424301b6e11ad6c5572ff8
Atiyah <a.m.g.elsheikh@gmail.com>
2022-06-23 19:08:51 +0100

    DoDeath year < 1950

--- src/lpm/Simulate.jl
+++ src/lpm/Simulate.jl
@@ -5,13 +5,72 @@
 
 module Simulate
 
-using SocialAgents: Person 
-using SocialAgents: isMale, isFemale
+using SocialAgents: Person  
+using SocialAgents: isMale, isFemale, isSingle
+using SocialAgents: removeOccupant!, resolvePartnership!
 using SocialABMs: SocialABM, allagents
 using Utilities: age2yearsmonths
+using LoneParentsModel.Declare: LPMUKDemographyOpt
+
 export doDeaths!
 
-function doDeaths!(population::SocialABM{Person}) # agent_step / model_step? 
+
+function deathProbability(baseRate,person,parameters) 
+    #=
+        Not realized yet  / to be realized in another module? 
+
+        classRank = person.classRank
+        if person.status == 'child' or person.status == 'student':
+            classRank = person.parentsClassRank
+    =# 
+
+    @assert isMale(person) || isFemale(person) # Assumption  
+    mortalityBias = isMale(person) ? parameters[:maleMortalityBias] : 
+                                     parameters[:femaleMortalityBias] 
+
+    #= 
+    To be integrated in class modules 
+    a = 0
+    for i in range(int(self.p['numberClasses'])):
+        a += self.socialClassShares[i]*math.pow(mortalityBias, i)
+    =# 
+
+    #=
+    if a > 0:
+        lowClassRate = baseRate/a
+        classRate = lowClassRate*math.pow(mortalityBias, classRank)
+        deathProb = classRate
+           
+        b = 0
+        for i in range(int(self.p['numCareLevels'])):
+            b += self.careNeedShares[classRank][i]*math.pow(self.p['careNeedBias'], (self.p['numCareLevels']-1) - i)
+                
+        if b > 0:
+            higherNeedRate = classRate/b
+            deathProb = higherNeedRate*math.pow(self.p['careNeedBias'], (self.p['numCareLevels']-1) - person.careNeedLevel) # deathProb
+    =#
+
+    # assuming it is just one class and without care need, 
+    # the above code translates to: 
+
+    deathProb = baseRate * mortalityBias 
+
+        ##### Temporarily by-passing the effect of Unmet Care Need   #############
+        
+#        The following code is already commented in the python code 
+#        a = 0
+#        for x in classPop:
+#            a += math.pow(self.p['unmetCareNeedBias'], 1-x.averageShareUnmetNeed)
+#        higherUnmetNeed = (classRate*len(classPop))/a
+#        deathProb = higherUnmetNeed*math.pow(self.p['unmetCareNeedBias'], 1-shareUnmetNeed)            
+     
+
+    deathProb 
+end # function deathProb
+
+
+function doDeaths!(population::SocialABM{Person};
+                   verbose = true, sleeptime=0) 
 
     parameters = population.properties 
 
@@ -19,21 +78,20 @@ function doDeaths!(population::SocialABM{Person}) # agent_step / model_step?
     currmonth = currmonth + 1 
 
     people = allagents(population)
-    livingPeople = [person for person in people if person.info.alive]
 
-    #=
-    println("living people $(length(livingPeople)) sample \n : ") 
-    println(livingPeople[1:10]) 
-    println(livingPeople[1].info.age)
-    println(typeof(livingPeople[1].info.age))
-    @assert typeof(livingPeople[1].info.age) == Rational{Int64} 
-    sleep(1)
-    =# 
+    livingPeople = typeof(population.properties[:example]) == LPMUKDemographyOpt ? 
+        people : [person for person in people if person.info.alive]
+
+    @assert length(livingPeople) > 0 ? 
+        typeof(livingPeople[1].info.age) == Rational{Int64} :
+        true  # Assumption
 
     for person in livingPeople
 
         @assert isMale(person) || isFemale(person) # Assumption 
         age = person.info.age 
+        dieProb = 0
+        lifeExpectancy = 0
 
         if curryear >= 1950 
 
@@ -50,16 +108,51 @@ function doDeaths!(population::SocialABM{Person}) # agent_step / model_step?
                             exp(age / parameters[:maleAgeScaling])  * parameters[:maleAgeDieProb] : 
                             exp(age / parameters[:femaleAgeScaling]) * parameters[:femaleAgeDieProb]
             rawRate = parameters[:baseDieProb] + babyDieProb + ageDieProb
-
+            
             #=
-            Not realized yet: 
+            Not realized yet 
             classPop = [x for x in self.pop.livingPeople 
                               if x.careNeedLevel == person.careNeedLevel]
+            Classes to be considered in a different module 
             =#
 
-        end
+            dieProb =  deathProbability(rawRate,person,parameters)
+            lifeExpectancy = max(90 - age, 5 // 1)  ## ??? 
+
+            #=
+            The following is uncommented code in the original code
+            #### Temporarily by-passing the effect of unmet care need   ######
+            # dieProb = self.deathProb_UCN(rawRate, person.parentsClassRank, person.careNeedLevel, person.averageShareUnmetNeed, classPop)
+            =# 
+
+            if rand() < dieProb && rand(1:12) == currmonth
+                if verbose 
+                    y, m = age2yearsmonths(age)
+                    println("person $(person.id) died year $(curryear) with age of $y")
+                    sleep(sleeptime) 
+                end
+                person.info.alive = false 
+                # person.deadYear = self.year
+                # deaths[person.classRank] += 1
+                false ? population.variables[:numberDeaths] += 1 : nothing # Temporarily this way till realized 
+                removeOccupant!(person.pos,person)
+                isSingle(person) ?
+                    nothing :  
+                    resolvePartnership!(person.kinship.partner,person)
+            end # rand
+
+        end # currYear < 1950 
         
-    end # for livingPeople 
-end
+    end # for livingPeople
+    
+    if verbose
+        println("number of living people : $(length(livingPeople)) from $(length(people))") 
+        sleep(sleeptime)
+    end 
+
+end # function doDeaths!
+
+
+
 
 end # Simulate 
