ce8cd4fd6f5c6293db1cddac2c93c2ae681540f7
Atiyah <a.m.g.elsheikh@gmail.com>
2022-07-27 13:13:26 +0100

    remove keyword argument

--- src/agents/BasicInfoM.jl
+++ src/agents/BasicInfoM.jl
@@ -36,7 +36,7 @@ alive(person::BasicInfo) = person.alive
 setDead!(person::BasicInfo) = person.alive = false
 
 "increment an age for a person to be used in typical stepping functions"
-agestep!(person::BasicInfo; dt=1//12) = person.age += dt  
+agestep!(person::BasicInfo, dt=1//12) = person.age += dt  
 
 "increment an age for a person to be used in typical stepping functions"
 function agestepAlive!(person::BasicInfo, dt=1//12) 
