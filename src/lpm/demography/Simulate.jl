05b4c6970c11da22dbbdb51eaeff8c02b36cd291
Atiyah <a.m.g.elsheikh@gmail.com>
2022-08-29 16:56:49 +0100

    assumption that all individuals are already alive

--- src/lpm/demography/Simulate.jl
+++ src/lpm/demography/Simulate.jl
@@ -62,16 +62,16 @@ function deathProbability(baseRate,person,parameters)
     deathProb 
 end # function deathProb
 
-""
+"evaluate death events in a population"
 function doDeaths!(;people,parameters,data,currstep,verbose=true,sleeptime=0)
 
     (curryear,currmonth) = date2yearsmonths(Rational(currstep))
-    currmonth = currmonth + 1 
+    currmonth = currmonth + 1 # adjusting 0:11 => 1:12 
     numDeaths = 0
 
     for person in people 
 
-        # @assert person.info.alive      
+        @assert alive(person)       
         @assert isMale(person) || isFemale(person) # Assumption 
         
         agep = age(person)             
@@ -115,7 +115,7 @@ function doDeaths!(;people,parameters,data,currstep,verbose=true,sleeptime=0)
         # dieProb = self.deathProb_UCN(rawRate, person.parentsClassRank, person.careNeedLevel, person.averageShareUnmetNeed, classPop)
         =# 
         
-        if rand() < dieProb && rand(1:12) == currmonth && alive(person)   
+        if rand() < dieProb && rand(1:12) == currmonth 
             if verbose 
                 y, m = date2yearsmonths(agep)
                 println("person $(person.id) died year $(curryear) with age of $y")
@@ -139,7 +139,151 @@ function doDeaths!(;people,parameters,data,currstep,verbose=true,sleeptime=0)
     end 
 
     (numberDeaths = numDeaths)   
-end
+end  # function doDeaths! 
+
+"evaluate births events in a population"
+function doBirths!(;people,parameters,data,currstep,verbose=true,sleeptime=0)
+
+    (curryear,currmonth) = date2yearsmonths(Rational(currstep))
+    currmonth = currmonth + 1   # adjusting 0:11 => 1:12 
+    numBirths = 0
+
+#=
+
+ preBirth = len(self.pop.livingPeople)
+        
+        marriedLadies = 0
+        adultLadies = 0
+        births = [0, 0, 0, 0, 0]
+        marriedPercentage = []
+        
+        allFemales = [x for x in self.pop.livingPeople if x.sex == 'female']
+        adultWomen = [x for x in self.pop.livingPeople if x.sex == 'female' and x.age >= self.p['minPregnancyAge']]
+        notFertiledWomen = [x for x in adultWomen if x.age > self.p['maxPregnancyAge']]
+        
+        print 'Number of adult women: ' + str(len(allFemales))
+        print 'Number of adult women: ' + str(len(adultWomen))
+        print 'Not fertile women: ' + str(len(notFertiledWomen))
+        
+        womenOfReproductiveAgeButNotMarried = [x for x in self.pop.livingPeople
+                                  if x.sex == 'female'
+                                  and x.age >= self.p['minPregnancyAge']
+                                  and x.age <= self.p['maxPregnancyAge']
+                                  and x.partner == None]
+        
+        print 'Not married fertile women: ' + str(len(womenOfReproductiveAgeButNotMarried))
+        
+        womenOfReproductiveAge = [x for x in self.pop.livingPeople
+                                  if x.sex == 'female'
+                                  and x.age >= self.p['minPregnancyAge']
+                                  and x.age <= self.p['maxPregnancyAge']
+                                  and x.partner != None]
+        
+        adultLadies_1 = [x for x in adultWomen if x.classRank == 0]   
+        marriedLadies_1 = len([x for x in adultLadies_1 if x.partner != None])
+        if len(adultLadies_1) > 0:
+            marriedPercentage.append(marriedLadies_1/float(len(adultLadies_1)))
+        else:
+            marriedPercentage.append(0)
+        adultLadies_2 = [x for x in adultWomen if x.classRank == 1]    
+        marriedLadies_2 = len([x for x in adultLadies_2 if x.partner != None])
+        if len(adultLadies_2) > 0:
+            marriedPercentage.append(marriedLadies_2/float(len(adultLadies_2)))
+        else:
+            marriedPercentage.append(0)
+        adultLadies_3 = [x for x in adultWomen if x.classRank == 2]   
+        marriedLadies_3 = len([x for x in adultLadies_3 if x.partner != None]) 
+        if len(adultLadies_3) > 0:
+            marriedPercentage.append(marriedLadies_3/float(len(adultLadies_3)))
+        else:
+            marriedPercentage.append(0)
+        adultLadies_4 = [x for x in adultWomen if x.classRank == 3]  
+        marriedLadies_4 = len([x for x in adultLadies_4 if x.partner != None])   
+        if len(adultLadies_4) > 0:
+            marriedPercentage.append(marriedLadies_4/float(len(adultLadies_4)))
+        else:
+            marriedPercentage.append(0)
+        adultLadies_5 = [x for x in adultWomen if x.classRank == 4]   
+        marriedLadies_5 = len([x for x in adultLadies_5 if x.partner != None]) 
+        if len(adultLadies_5) > 0:
+            marriedPercentage.append(marriedLadies_5/float(len(adultLadies_5)))
+        else:
+            marriedPercentage.append(0)
+        
+        # print(marriedPercentage)
+        
+#        for person in self.pop.livingPeople:
+#           
+#            if person.sex == 'female' and person.age >= self.p['minPregnancyAge']:
+#                adultLadies += 1
+#                if person.partner != None:
+#                    marriedLadies += 1
+#        marriedPercentage = float(marriedLadies)/float(adultLadies)
+        
+        print 'Number of reproductive women: ' + str(len(womenOfReproductiveAge))
+        
+        for woman in womenOfReproductiveAge:
+            
+            womanClassRank = woman.classRank
+            if woman.status == 'student':
+                womanClassRank = woman.parentsClassRank
+
+            if self.year < 1951:
+                rawRate = self.p['growingPopBirthProb']
+                birthProb = self.computeBirthProb(self.p['fertilityBias'], rawRate, womanClassRank)
+            else:
+                rawRate = self.fert_data[(self.year - woman.birthdate)-16, self.year-1950]
+                birthProb = self.computeBirthProb(self.p['fertilityBias'], rawRate, womanClassRank)/marriedPercentage[womanClassRank]
+                
+            # birthProb = self.computeBirthProb(self.p['fertilityBias'], rawRate, woman.classRank)
+            
+            #baseRate = self.baseRate(self.socialClassShares, self.p['fertilityBias'], rawRate)
+            #fertilityCorrector = (self.socialClassShares[woman.classRank] - self.p['initialClassShares'][woman.classRank])/self.p['initialClassShares'][woman.classRank]
+            #baseRate *= 1/math.exp(self.p['fertilityCorrector']*fertilityCorrector)
+            #birthProb = baseRate*math.pow(self.p['fertilityBias'], woman.classRank)
+            if birthProb <= 0.0:
+                print 'Error: birth prob is zeo/negative!'
+                sys.exit()
+                
+            if np.random.random() < birthProb and np.random.choice([x+1 for x in range(12)]) == month:
+                # (self, mother, father, age, birthYear, sex, status, house,
+                # classRank, sec, edu, wage, income, finalIncome):
+                parentsClassRank = max([woman.classRank, woman.partner.classRank])
+                baby = Person(woman, woman.partner, self.year, 0, 'random', woman.house, woman.sec, -1, parentsClassRank, 0, 0, 0, 0, 0, 0, 'child', False, 0, month)
+                self.pop.allPeople.append(baby)
+                self.pop.livingPeople.append(baby)
+                woman.house.occupants.append(baby)
+                woman.children.append(baby)
+                woman.partner.children.append(baby)
+                woman.maternityStatus = True
+                # woman.weeklyTime = [[0]*12+[1]*12, [0]*12+[1]*12, [0]*12+[1]*12, [0]*12+[1]*12, [0]*12+[1]*12, [0]*12+[1]*12, [0]*12+[1]*12]
+                woman.weeklyTime = [[1]*24, [1]*24, [1]*24, [1]*24, [1]*24, [1]*24, [1]*24]
+                woman.workingHours = 0
+                woman.maxWeeklySupplies = [0, 0, 0, 0]
+                woman.residualDailySupplies = [0]*7
+                woman.residualWeeklySupplies = [x for x in woman.maxWeeklySupplies]
+                woman.residualWorkingHours = 0
+                woman.availableWorkingHours = 0
+                woman.potentialIncome = 0
+                woman.income = 0
+                if woman.house == self.displayHouse:
+                    messageString = str(self.year) + ": #" + str(woman.id) + " had a baby, #" + str(baby.id) + "." 
+                    self.textUpdateList.append(messageString)
+                    
+                    with open(os.path.join(policyFolder, "Log.csv"), "a") as file:
+                        writer = csv.writer(file, delimiter = ",", lineterminator='\r')
+                        writer.writerow([self.year, messageString])
+                    
+        postBirth = len(self.pop.livingPeople)
+        numberBirths = postBirth - preBirth
+        print 'The number of births is: ' + str(numberBirths)
+
+
+=# 
+
+
+end  # function doBirths! 
+
 
 
 end # module Simulate 
