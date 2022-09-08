dc8678a3328d62e6d46d2eafc5f3440d793a5393
Atiyah <a.m.g.elsheikh@gmail.com>
2022-09-08 12:49:20 +0100

    verify version number

--- MainMALPM.jl
+++ MainMALPM.jl
@@ -12,7 +12,7 @@ from REPL execute it using
 
 include("./loadLibsPath.jl")
 
-using MultiAgents: MultiABM, initMultiAgents
+using MultiAgents: MultiABM, initMultiAgents, MAVERSION
  
 using LPM.Parameters.Loaders:    loadUKDemographyPars
 
@@ -23,7 +23,8 @@ using MALPM.Demography.SimSetup:   loadSimulationParameters
 using MultiAgents: MABMSimulation
 using MultiAgents: run! 
 
-initMultiAgents()     # reset agents counter
+initMultiAgents()                 # reset agents counter
+@assert MAVERSION == v"0.2.2"   # ensure MultiAgents.jl latest update 
 
 ukDemographyPars = loadUKDemographyPars() 
 
