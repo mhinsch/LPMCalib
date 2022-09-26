526a2b01d8b0c507ee47ba2e2c31150647c4e761
Martin Hinsch <hinsch.martin@gmail.com>
2022-09-26 13:40:45 +0100

    analysis + graph, solves issues #93, #95

--- main_gui.jl
+++ main_gui.jl
@@ -5,6 +5,7 @@ using Raylib: rayvector
 const RL = Raylib
 
 include("mainHelpers.jl")
+include("analysis.jl")
 
 include("src/RayGUI/render.jl")
 
@@ -26,12 +27,14 @@ function main()
 
     camera = RL.RayCamera2D(
         rayvector(screenWidth/2, screenHeight/2),
-        rayvector(500, 500),
+        rayvector(screenWidth/2, screenHeight/2),
+        #rayvector(500, 500),
         0,
         1
     )
 
-    graph = Graph{Float64}(RL.BLUE)
+    graph_pop = Graph{Float64}(RL.BLUE)
+    graph_hhs = Graph{Float64}(RL.WHITE)
 
     pause = false
     time = Rational(simPars.startTime)
@@ -40,7 +43,9 @@ function main()
         if !pause && time <= simPars.finishTime
             step!(model, time, simPars, pars)
             time += simPars.dt
-            add_value!(graph, rand())
+            data = observe(Data, model)
+            add_value!(graph_pop, data.N)
+            add_value!(graph_hhs, data.hh_size.mean)
         end
 
         RL.BeginDrawing()
@@ -50,7 +55,7 @@ function main()
         RL.BeginMode2D(camera)
         
         drawModel(model, (0, 0), (50, 50), (2, 2))
-        draw_graph(1000, 0, 600, 900, [graph])
+        draw_graph(1000, 0, 600, 900, [graph_pop, graph_hhs], false)
         
         RL.EndMode2D()
 
