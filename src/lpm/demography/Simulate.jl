35def633a9d4b8640a17b50d17a5f6a7b476bcaf
Atiyah <a.m.g.elsheikh@gmail.com>
2022-09-29 15:17:08 +0100

    Initial draft

--- src/lpm/demography/Simulate.jl
+++ src/lpm/demography/Simulate.jl
@@ -6,7 +6,8 @@ module Simulate
 
 include("simulate/allocate.jl")
 include("simulate/death.jl")
-include("simulate/birth.jl")         
+include("simulate/birth.jl")  
+include("simulate/divorce.jl")       
 include("simulate/ageTransition.jl")
 include("simulate/socialTransition.jl")
 
---
+++ src/lpm/demography/simulate/divorce.jl
@@ -0,0 +1,135 @@
+
+
+function divorceProbability(rawRate, divorceBias) # ,classRank) 
+    #=
+     def computeSplitProb(self, rawRate, classRank):
+        a = 0
+        for i in range(int(self.p['numberClasses'])):
+            a += self.socialClassShares[i]*math.pow(self.p['divorceBias'], i)
+        baseRate = rawRate/a
+        splitProb = baseRate*math.pow(self.p['divorceBias'], classRank)
+        return splitProb
+    =# 
+    rawRate * divorceBias 
+end 
+
+# Internal function 
+function manSubjectToDivorce!(man,parameters,data,time;
+                                verbose,sleeptime,checkassumption)
+        
+    (curryear,currmonth) = date2yearsmonths(time)
+    currmonth += 1 # adjusting 0:11 => 1:12
+                                
+    agem = age(man) 
+    if checkassumption 
+        @assert isMale(man) 
+        @assert !isSingle(man)
+        @assert typeof(agem) == Rational{Int}
+    end
+    
+    ## This is here to manage the sweeping through of this parameter
+    ## but only for the years after 2012
+    if curryear < parameter.thePresent 
+        # Not sure yet if the following is parameter or data 
+        rawRate = parameters.basicDivorceRate * parameters.divorceModifierByDecade[trunc(Int, agem / 10]
+    else 
+        rawRate = parameters.variableDivorce * parameters.divorceModifierByDecade[trunc(Int, agem / 10]           
+    end
+
+    divorceProb = divorceProbability(rawRate,parameters,data,time) # TODO , man.classRank)
+
+    if rand() < divorceProb && rand(1:12) == currmonth
+        resolvePartnership!(man, partner(man))
+        
+        #=
+        man.yearDivorced.append(self.year)
+        wife.yearDivorced.append(self.year)
+        if wife.status == 'student':
+            wife.independentStatus = True
+            self.startWorking(wife)
+        =# 
+
+        attachedChildren = Person[]
+        for child in children(person)
+            if !alive(child) continue end 
+            if father(child) == man && mother(child) != partner(man)
+                append!(attachedChildren,child)
+            else 
+                if rand() < parameters.probChildrenWithFather
+                    append!(attachedChildren,child)
+                end
+            end 
+        end # for 
+
+         
+
+        return true 
+    end
+
+    #=
+    if np.random.random() < splitProb and np.random.choice([x+1 for x in range(12)]) == month:
+        # man.children = []
+        
+        
+        self.divorceTally += 1
+        distance = random.choice(['near','far'])
+        if man.house == self.displayHouse:
+            messageString = str(self.year) + ": #" + str(man.id) + " splits with #" + str(wife.id) + "."
+            self.textUpdateList.append(messageString)
+            
+            with open(os.path.join(policyFolder, "Log.csv"), "a") as file:
+                writer = csv.writer(file, delimiter = ",", lineterminator='\r')
+                writer.writerow([self.year, messageString])
+            
+    =# 
+
+    false 
+end 
+
+
+function doDivorces!(people;parameters,data,time,
+                        verbose=true,sleeptime=0.0,checkassumption=true) 
+
+    marriedMens = [ man for man in people if isMale(man) && !isSingle(man) ]
+
+    divorced = Person[] 
+    
+    for man in marriedMens 
+        if manSubjectToDivorce!(man, parameters, data, time, 
+                                verbose = verbose, 
+                                sleeptime = sleeptime, 
+                                checkassumption = checkassumption )
+            divorced.append!([man, partner(man)]) 
+        end 
+    end 
+
+    if verbose 
+        println("# of divorced in current iteration $(length(divorced))")
+        sleeptime(sleeptime)
+    end
+    
+    divorced 
+end
+
+
+#=
+def doDivorces(self, policyFolder, month):
+        menInRelationships = [x for x in self.pop.livingPeople if x.sex == 'male' and x.partner != None ]
+        for man in menInRelationships:
+            
+            
+                
+            
+                # manChildren = [x for x in man.children if x.dead == False and x.house == man.house and x.father == man and x.mother != wife]
+                
+ 
+                
+#                for x in manChildren:
+#                    if x not in man.house.occupants:
+#                        print 'Error in doDivorce: man children not in house'
+#                        sys.exit()
+                        
+                peopleToMove = [man]
+                peopleToMove += manChildren
+                self.findNewHouse(peopleToMove,distance, policyFolder)
+=# 
