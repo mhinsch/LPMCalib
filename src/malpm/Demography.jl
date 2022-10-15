ca8583ebce6329580eae83c5883a7b2a7a70b676
Atiyah <a.m.g.elsheikh@gmail.com>
2022-10-15 17:40:12 +0100

    re-wrting (under construction)

--- src/malpm/Demography.jl
+++ src/malpm/Demography.jl
@@ -1,8 +1,13 @@
 module Demography
 
+
+
+    #=
     include("./demography/Create.jl") 
     include("./demography/Initialize.jl")  
     include("./demography/Simulate.jl")
     include("./demography/SimSetup.jl")   
+    =# 
+
 
 end # Demography
