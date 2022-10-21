53540199602186a2f17a7303d61ad46d1c6ae0fd
Atiyah <a.m.g.elsheikh@gmail.com>
2022-10-21 13:44:53 +0100

    no longer needed

--- MainMALPM.jl
+++ MainMALPM.jl
@@ -10,7 +10,24 @@ from REPL execute it using
 > include("MainMALPM.jl")
 """
 
+using Random
+
 include("./loadLibsPath.jl")
+
+addToLoadPath!("src")
+addToLoadPath!("src/multiagents") 
+addToLoadPath!("../MultiAgents.jl")
+
+include("mainHelpers.jl")
+
+const simPars, pars = loadParameters(ARGS) 
+
+Random.seed!(simPars.seed)
+
+const model = setupModel(pars)
+
+const logfile = setupLogging(simPars)
+
 
 using MultiAgents: initMultiAgents, MAVERSION
 using MultiAgents: AbstractMABM, ABMSimulation 
@@ -20,46 +37,17 @@ initMultiAgents()                 # reset agents counter
 @assert MAVERSION == v"0.3"   # ensure MultiAgents.jl latest update 
 
 
-if !occursin("multiagents",LOAD_PATH)
-    push!(LOAD_PATH, "src/multiagents") 
-end
 
 using MALPM.Demography: MAModel, LPMUKDemography, LPMUKDemographyOpt 
 using MALPM.Demography.SimSetup: setup! 
 
 
-# using LPM.ParamTypes.Loaders:    loadUKDemographyPars
-
-#=
-using MALPM.Demography.Create:     LPMUKDemography, LPMUKDemographyOpt, createUKDemography 
-using MALPM.Demography.Initialize: initializeDemography!
-using MultiAgents: run! 
-=# 
-
-include("mainHelpersModel.jl")
-
-const simPars, pars = getParameters() 
-
-const model = setupModel(pars)
-
 ukDemography = MAModel(model,pars)
-
-if simPars.verbose
-    @show "Town Samples: \n"     
-    @show ukDemography.towns.agentsList[1:10]
-    println(); println(); 
-                        
-    @show "Houses samples: \n"      
-    @show ukDemography.houses.agentsList[1:10]
-    println(); println(); 
-                        
-    @show "population samples : \n" 
-    @show ukDemography.pop.agentsList[1:10]
-    println(); println(); 
-end 
 
 lpmDemographySim = ABMSimulation(simPars,setupEnabled = false)
 setup!(lpmDemographySim,LPMUKDemography()) 
  
 # Execution 
 @time run!(ukDemography,lpmDemographySim,LPMUKDemographyOpt())
+
+close(logfile)
