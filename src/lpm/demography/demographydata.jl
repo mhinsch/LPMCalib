82338fc8d256652c2d99fdee8bae59ccfd791203
Atiyah <a.m.g.elsheikh@gmail.com>
2022-10-26 10:22:15 +0100

    Demography data type

--- src/lpm/demography/demographydata.jl
+++ src/lpm/demography/demographydata.jl
@@ -2,15 +2,20 @@ using CSV
 using Tables
 
 
-export loadDemographyData
+export loadDemographyData, DemographyData
 
+struct DemographyData
+    fertility   :: Matrix{Float64}
+    deathFemale :: Matrix{Float64}
+    deathMale   :: Matrix{Float64}  
+end
 
 function loadDemographyData(fertFName, deathFFName, deathMFName) 
     fert = CSV.File(fertFName, header=0) |> Tables.matrix
     deathFemale = CSV.File(deathFFName, header=0) |> Tables.matrix
     deathMale = CSV.File(deathMFName, header=0) |> Tables.matrix
 
-    (fertility=fert,deathFemale=deathFemale,deathMale=deathMale)
+    DemographyData(fert,deathFemale,deathMale)
 end
 
 function loadDemographyData(datapars)
