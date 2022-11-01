60fe7eaa2662a02d132e5eea95d117eab3e530fb
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-01 11:11:26 +0000

    allowing manual setting of significant parameters from REPL

--- mainMA.jl
+++ mainMA.jl
@@ -10,13 +10,24 @@ from REPL execute it using
 
 include("mainMAHelpers.jl")
 
-mainConfig = Light() 
+mainConfig = Light()    # no input files, logging or flags (REPL Exec.) 
 # mainConfig = WithInputFiles()
 
-lpmExample = LPMUKDemography()    # remove deads
-# lpmExample = LPMUKDemographyOpt()   # don't remove deads 
+# lpmExample = LPMUKDemography()    # remove deads
+lpmExample = LPMUKDemographyOpt()   # don't remove deads 
 
 const simPars, pars = loadParameters(mainConfig) 
+
+# Most significant simulation and model parameters 
+# The following works only with Light() configuration
+#   useful when executing from REPL
+if mainConfig == Light() 
+    simPars.seed = 0; seed!(simPars)
+    simPars.verbose = false  
+    simPars.checkassumption = false 
+    simPars.sleeptime = 0.0 
+    pars.poppars.initialPop = 500
+end
 
 const model = setupModel(pars)
 
