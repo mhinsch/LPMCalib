c51e94d6e2e4501aaf4a6bb899aefc75b5c4e4c1
Martin Hinsch <hinsch.martin@gmail.com>
2022-11-17 06:56:27 +0000

    run script

---
+++ run_abc.jl
@@ -0,0 +1,73 @@
+using Distributions
+using CSV
+using DataFrames
+using Random
+
+include("main_dist.jl")
+include("abc_hpc/abc/ownabc.jl")
+
+
+function run_sim_locally(args)
+    # sadly our simulation is not thread-safe so we have to run it via its own julia process
+    res = readchomp(`julia main_dist.jl $(split(args))`)
+    
+    parse(Float64, res)
+end
+
+# this works when using ThreadSafeDict for memoization but is sadly much, much slower than running externally
+function run_sim_threaded(args)
+    distance(split(args))
+end
+
+function simulate(param_values)
+    @assert length(names) == length(param_values)
+    
+    # TODO seeding
+    args = "--datadir LPM/data -P \"\""
+    
+    for (name, value) in zip(names, param_values)
+        args *= " --$name $value" 
+    end
+
+    run_sim_locally(args)
+    #run_sim_threaded(args)
+end
+
+function readParamConfig(fname)
+    pconf = CSV.read(fname, DataFrame)
+    
+    v_min = pconf[!, :Min]
+    v_max = pconf[!, :Max]
+    
+    priors = [Uniform(mi, ma) for (mi, ma) in zip(v_min, v_max)]
+    
+    String.(pconf[!, :Parameter]), priors
+end
+    
+
+function run_abc(n_iters)
+    names, priors = readParamConfig("params/parameters.tsv");
+
+    noise = [0.05 for p in priors];
+
+    particles = Particle[]
+    remv = PropRemover(0.5)
+    creat = OwnCreator(true, 1.0, priors, noise)
+
+    result = nothing
+    #abc(priors, dist, 200, 0.5, noise, 10, verbose=true, scale_noise=true, parallel=true)
+    #abc(priors, dist, 4, 0.5, noise, 2, verbose=true, scale_noise=true, parallel=true)
+    for i in 1:n_iters
+        println("starting iteration $i")
+        result = abc_iter!(particles, simulate, 200, remv, creat, verbose=true, parallel = true)
+        #sort!(particles, by=p->p.dist)
+        #push!(meds, particles[end ÷ 2].dist)
+        #for i in 1:4
+        #    push!(devs[i], std(map(p->p.params[i], particles)))
+        #end
+    end
+
+    result
+end
+
+
