a28f2aa0043e30b5e29d3ff79b4498bccc3b3598
Martin Hinsch <hinsch.martin@gmail.com>
2022-12-07 10:54:08 +0000

    show income deciles

--- analysis.jl
+++ analysis.jl
@@ -5,6 +5,21 @@ const MVA = MeanVarAcc{Float64}
 # maximum, minimum
 const MMA = MaxMinAcc{Float64}
 
+function income_deciles(pop)
+    incomes = [ income(p) for p in pop ]
+    sort!(incomes)
+
+    dec_size = length(pop) ÷ 10
+    inc_decs = zeros(10)
+    
+    for i in 1:(10*dec_size)
+        inc = incomes[i]
+        inc_decs[(i-1) ÷ dec_size + 1] += inc
+    end
+
+    inc_decs ./ dec_size
+end
+
 @observe Data model begin
     @for house in model.houses begin
         # format:
@@ -18,11 +33,14 @@ const MMA = MaxMinAcc{Float64}
         @stat("single", CountAcc) <| (age(person) > 18 && isSingle(person))
         @stat("alive", CountAcc) <| true
         @stat("class", HistAcc(0.0, 1.0, 4.0)) <| Float64(classRank(person))
+        @stat("income", MVA) <| income(person)
     end
 
     @for person in Iterators.filter(p->alive(p)&&isFemale(p), model.pop) begin
         @stat("f_status", HistAcc(0, 1, 5)) <| Int(status(person))
     end
+
+    @record "income_deciles" Vector{Float64} income_deciles(model.pop)
 
     @for person in Iterators.filter(p->alive(p)&&isMale(p), model.pop) begin
         @stat("m_status", HistAcc(0, 1, 5)) <| Int(status(person))
