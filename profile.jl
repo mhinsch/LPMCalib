de6334447394e110299e7e553a2280dbc9db4009
Martin Hinsch <hinsch.martin@gmail.com>
2023-03-15 19:14:16 +0000

    profiling helper script

---
+++ profile.jl
@@ -0,0 +1,29 @@
+using ProfileView
+
+include("lpm.jl")
+include("analysis.jl")
+
+const simPars, pars = loadParameters(ARGS)
+
+const model = setupModel(pars)
+ 
+t = Rational(pars.poppars.startTime)
+
+function run_until(t_end)
+    while true
+        global model, t, pars, simPars
+        stepModel!(model, t, pars)
+        t += simPars.dt
+        if t >= t_end
+            break
+        end
+    end
+end   
+
+
+function run_steps(n)
+    for i in 1:n
+        global model, t, pars, simPars
+        stepModel!(model, t, pars)
+    end
+end
