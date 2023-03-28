dd7368cfa4240be34a6a28e5d59cb3ce4de97e19
Martin Hinsch <hinsch.martin@gmail.com>
2023-03-28 09:47:32 +0100

    analysis.jl for new age data

--- valid_runs/analysis.jl
+++ valid_runs/analysis.jl
@@ -99,7 +99,7 @@ end
 
     @for person in I.filter(p->isFemale(p) && !isSingle(p), model.pop) begin
         agediff = Float64(age(partner(person)) - age(person))
-        # -6, so that the lowest bin is [-Inf, -5]
-        @stat("couple_age_diff", HistAcc(-6.0, 1.0, count_below_min=true)) <| agediff
+        # -20.5, so that the lowest bin is [-Inf, -19.5]
+        @stat("couple_age_diff", HistAcc(-20.5, 1.0, count_below_min=true)) <| agediff
     end
 end
